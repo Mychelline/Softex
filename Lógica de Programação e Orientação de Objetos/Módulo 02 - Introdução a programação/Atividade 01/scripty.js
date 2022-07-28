9fd3facb94dc7df38dd0c96ff052d4c0abb69265
WandeilsonFerreira <wandeilson.ferreira@gmail.com>
2022-07-28 07:46:19 -0300

    Resolução da segunda atividade da unidade dois

--- Lógica de Programação e Orientação de Objetos/Módulo 02 - Introdução a programação/Atividade 01/scripty.js
+++ Lógica de Programação e Orientação de Objetos/Módulo 02 - Introdução a programação/Atividade 01/scripty.js
@@ -1,22 +1,23 @@
 let input = require('prompt-sync')()
 
 console.log('Digite o nome do aluno: ')
-var nomeAluno = input();
+var nomeAluno = input()
 
 console.log('Digite a primeira nota: ')
-var notaUm = input();
+var notaUm = input()
 
 console.log('Digite a segunda nota: ')
-var notaDois = input();
+var notaDois = input()
 
 console.log('Digite o numero de faltas: ')
-var quantidadeFaltas = input();
+var quantidadeFaltas = input()
 
-var calcMedia = (notaUm + notaDois) / 2;
-var media = calcMedia < 7 ? 'Reprovado' : 'Aprovado';
-var faltas = quantidadeFaltas > 3 ? 'Reprovado' : 'Aprovado';
+var calcMedia = (Number(notaUm) + Number(notaDois)) / 2
+
+var media = calcMedia < 7 ? 'Reprovado' : 'Aprovado'
+var faltas = quantidadeFaltas > 3 ? 'Reprovado' : 'Aprovado'
 var status =
-  (media == 'Aprovado') & (faltas == 'Aprovado') ? 'Aprovado' : 'Reprovado';
+  (media == 'Aprovado') & (faltas == 'Aprovado') ? 'Aprovado' : 'Reprovado'
 
-console.clear();
-console.log('O aluno ' + nomeAluno + ' está ' + status);
+console.clear()
+console.log('O aluno ' + nomeAluno + ' está ' + status)
